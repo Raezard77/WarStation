49986405465085c0490d2ef86b6bfb9147c0641d
Raezard77 <zartisian77@gmail.com>
2023-07-30 01:27:31 +0300

    build navbar - p9

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,9 +1,15 @@
 window.onload = (_) => {
     const burger = document.querySelector("#burger-menu");
     const navLinks = document.querySelector("#nav-ul");
-
+    
     burger.addEventListener("click", () => {
-        burger.classList.toggle("active");
-        navLinks.classList.toggle("shown");
+        burger.classList.toggle("burger-cross");
+        navLinks.classList.toggle("side-nav");
     });
+    
+    const ele = document.querySelector('#nav-ul li a:first-child')
+    ele.classList.add("page-selected")
+    navLinks.addEventListener("mouseover", _ => ele.classList.remove("page-selected"))
+    navLinks.addEventListener("mouseleave", _ => ele.classList.add("page-selected"))
 };
+// window.location.pathname
